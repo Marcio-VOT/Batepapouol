af233567806eb5f157a3ec93109b434c161e63d9
MarcioJrPC <marciofsantos65@gmail.com>
2022-10-29 23:40:25 -0300

    front melhorado/enviar mensagens/ajuste no filtro de mensagens privadas

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let promisse
 const mensagens = document.querySelector('.corpo');
 let nome = ""
 let mandaNome
-let msg = ""
+let msg = {
+    from:"", to:"", text:"",type:""
+}
 perguntaNome();
 chat();
 setInterval(chat, 3000);
@@ -23,14 +25,14 @@ function chat(){
 }
 function sucesso(resposta){
     resposta = resposta.data;
-    i = 0
+    i = 0;
     mensagens.innerHTML = "";
     while( i < resposta.length){
         // 1-- mensagem de entrando/saindo // 2-- mensagens globais// 3-- mensagens pessoais (só exibe se for pra vc msm)
         
         if(resposta[i].type == "message"){
             mensagens.innerHTML +=`
-            <div class="normal">
+            <div class="normal texto">
             <h1>
             (${resposta[i].time}) ${resposta[i].from} para ${resposta[i].to}: ${resposta[i].text}
             </h1>
@@ -39,16 +41,16 @@ function sucesso(resposta){
         }
         else if(resposta[i].type == "status"){
             mensagens.innerHTML +=`
-            <div class="status">
+            <div class="status texto">
             <h1>
             (${resposta[i].time}) ${resposta[i].from} ${resposta[i].text}
             </h1>
             </div>
             `
         }
-        else if(resposta[i].type == "private_message" && resposta[i].to == nome){
+        else if(resposta[i].type == "private_message" && resposta[i].to == nome.name){
             mensagens.innerHTML +=`
-            <div class="private">
+            <div class="private texto">
             <h1>
             (${resposta[i].time}) ${resposta[i].from} reservadamente para ${resposta[i].to}: ${resposta[i].text}
             </h1>
@@ -61,4 +63,19 @@ function sucesso(resposta){
 }
 function erro(){
     alert('erro');
+}
+function enviarMensagem(){
+    msg.from = nome.name;
+    msg.to = "Todos"
+    msg.text = document.getElementById('texto').value;
+    msg.type = "message"
+    if(msg.text != ""){
+        let postaMsg = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', msg);
+        postaMsg.catch(recarrega);
+        postaMsg.then(chat)
+    }
+    document.getElementById('texto').value = ""
+}
+function recarrega(){
+    window.location.reload();
 }
